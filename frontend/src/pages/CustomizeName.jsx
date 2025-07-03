6bb658918553182538447c8654161fd16eb14d57
Atidivya Kumar Patra <atidivya@gmail.com>
2025-07-03 15:51:16 +0100

    Update CustomizeName.jsx userContext

--- frontend/src/pages/CustomizeName.jsx
+++ frontend/src/pages/CustomizeName.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import { userDataContext } from "../context/userContext";
+import { userDataContext } from "../context/UserContext";
 import axios from "axios";
 import { MdKeyboardBackspace } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
