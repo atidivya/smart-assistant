adeecd1c48ecce49bdcb4bf69ea273646971c14e
Ati <atidivya@gmail.com>
2025-07-04 09:38:26 +0100

    Update vite.config.js base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://github.com/atidivya/smart-assistant/',
   server: {
     port: 5000,
   },
