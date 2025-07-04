5159c49bf5900bc2ef11abce8c1262d5af6a4e64
Ati <atidivya@gmail.com>
2025-07-04 09:55:11 +0100

    Update index.js back to default FE URL

--- backend/index.js
+++ backend/index.js
@@ -10,25 +10,10 @@ dontenv.config()
 
 const app = express()
 
-const allowedOrigins = [
-  "https://smart-assistant-3tjm.onrender.com",
-  "https://atidivya.github.io/smart-assistant/",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps, curl, Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
+app.use(cors({
+    origin: "https://smart-assistant-3tjm.onrender.com",
     credentials: true
-  })
-);
+}))
 
 const port = process.env.PORT || 8080
 
